9f452d1139f1cf920d2401a8b17d6da6a8dc6cb4
Sam Georges <sam@daftspunk.com>
2012-10-22 13:21:45 +1100

    Spruce up checkout start and progress pages

--- pages/store_checkout/page_store_checkout.php
+++ pages/store_checkout/page_store_checkout.php
@@ -1,10 +1,11 @@
-<div class="grid_16" id="cart_page">
-  <? if (Shop_Cart::get_item_total_num() != 0): ?>
-    <div id="checkout_page">
-      <? $this->render_partial('shop:checkout_partial') ?>
-    </div>
-  <? else: ?>
-    <p>Your cart is empty.</p>
-    <p><a class="link_button" href="<?= root_url('store')?>">Continue shopping</a></p>
-  <? endif ?>
+<div id="page_store_checkout">
+    <div class="page_header">
+        <h2><?= h($this->page->title) ?></h2>
+    </div>    
+    <? if (Shop_Cart::get_item_total_num() != 0): ?>
+        <div id="p_checkout_partial"><? $this->render_partial('shop:checkout_partial') ?></div>
+    <? else: ?>
+        <p>Your cart is empty.</p>
+        <p><a class="link_button" href="<?= root_url('store')?>">Continue shopping</a></p>
+    <? endif ?>
 </div>
--- pages/store_checkout_start/page_store_checkout_start.php
+++ pages/store_checkout_start/page_store_checkout_start.php
@@ -1,26 +1,29 @@
-<div class="grid_16" id="cart_page">
-  <? if (Shop_Cart::get_item_total_num() != 0): ?>
-    <div class="grid_5 alpha">
-      <div class="vert_separator_right">
-        <h5>Existing customers</h5>
-        <p>Please sign in using your existing account.</p>
-
-        <? $this->render_partial('account:login_form', array('redirect'=>root_url('store/checkout'))) ?>
-        <p>Don’t have an account? <a href="<?= root_url('account/login') ?>">Register!</a></p>
-      </div>
-    </div>
-    <div class="grid_11 omega">
-      <h5>New customers</h5>
-      <p>If you do not have an account and you do not want to register, you may checkout as a guest.</p>
-      <p>
-        <a class="button_control" href="<?= root_url('store/checkout') ?>">Checkout as Guest</a>
-      </p>
-
-      <h6>Why register?</h6>
-      <p>Registration allows you to avoid filling in billing and shipping forms every time you checkout on this website.</p>
-    </div>
-  <? else: ?>
-    <p>Your cart is empty.</p>
-    <p><a class="link_button" href="<?= root_url('store')?>">Continue shopping</a></p>
-  <? endif ?>
+<div id="page_store_checkout_start">
+    <? if (Shop_Cart::get_item_total_num() != 0): ?>
+        <div class="row">
+            <div class="four columns">
+                <div class="separator right">
+                    <h3>Existing customers</h3>
+                    <p>Please sign in with your existing account.</p>
+                    <? $this->render_partial('account:login_form', array('redirect'=>root_url('store/checkout'))) ?>
+                    <p>
+                        Don’t have an account? 
+                        <a href="<?= root_url('account/login') ?>">Register!</a>
+                    </p>
+                </div>
+            </div>
+            <div class="eight columns">
+                <h3>New customers</h3>
+                <p>If you do not have an account and you do not want to register, you may checkout as a guest.</p>
+                <p>
+                    <a class="button radius" href="<?= root_url('store/checkout') ?>">Checkout as Guest</a>
+                </p>
+                <h5>Why register?</h5>
+                <p>Registration allows you to avoid filling in billing and shipping forms every time you checkout on this website.</p>
+            </div>
+        </div>
+    <? else: ?>
+        <p>Your cart is empty.</p>
+        <p><a class="link_button" href="<?= root_url('store')?>">Continue shopping</a></p>
+    <? endif ?>
 </div>
