3739b039168987f780ec7731431d415dbcfc4076
Sam Georges <sam@daftspunk.com>
2013-02-23 11:33:43 +1100

    Update status bar code

--- resources/javascript/phpr.js
+++ resources/javascript/phpr.js
@@ -4,16 +4,16 @@
 
 Phpr.showLoadingIndicator = function() {
     var message = 'Processing...';
-    $(document).statusbar('option', {context: 'info', time: 999999, message:message});
+    $(document).statusbar('option', {position: 'top', context: 'info', time: 999999, message:message});
 };
 
 Phpr.hideLoadingIndicator = function() {
-    $(document).statusbar('remove_message');
+    $(document).statusbar('removeMessage');
 };
 
 Phpr.response.popupError = function() {
     var message = this.html.replace('@AJAX-ERROR@', '');
-    $(document).statusbar('option', {context: 'error', time: 10000, message:message});
+    $(document).statusbar('option', {position: 'top', context: 'error', time: 10000, message:message});
 };
 
 Phpr.options.extraFields = {
